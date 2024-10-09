9b838e760c76a2f192847f999b923f9c7b2799e0
Sarah Clarke <sarahclarke.dev@gmail.com>
2024-10-09 16:37:34 +1300

    TechList added

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -1,6 +1,9 @@
+import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import React from "react";
+import { MdCircle } from "react-icons/md";
 
 /**
  * Props for `TechList`.
@@ -15,14 +18,32 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="overflow-hidden"
     >
-      <Heading size="lg" as="h2"> 
+      <Bounded as="div">
+
+      <Heading size="xl"className="mb-8" as="h2"> 
         {slice.primary.heading}
       </Heading>
-{/* Map over the tech list instead of items - since update to Prismic */}
+      </Bounded>
+      
+      {/* Map over the tech list */}
       {slice.primary.tech_list?.map(({ tech_colour, tech_name }, index) => (
-        <div key={index}>
-          {tech_name} {tech_colour}
+        <div key={index} className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700">
+          {Array.from({ length: 15 }, (_, i) => (
+            <><span
+              key={i}
+              className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+              style={{
+                // Apply color only on the 7th occurrence
+                color: i === 7 && tech_colour ? tech_colour : "inherit"
+              }}
+            >
+              {tech_name}
+            </span><span className="text-3xl">
+                <MdCircle />
+              </span></>
+          ))}
         </div>
       ))}
     </section>
@@ -30,4 +51,6 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
 };
 
 export default TechList;
+
+
 
